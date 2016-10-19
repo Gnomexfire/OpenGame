c09fd810895f3840fdc9454cbb3f5cc533522774
hans Peter Arnt <hanspeterarnt@gmail.com>
2016-10-19 11:21:58 -0200

    ver1

---
+++ OpenGame/OpenGame/Scene/Level.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameEngine;
+using Microsoft.Xna.Framework;
+namespace OpenGame
+{
+    public class Level:SceneBase
+    {
+        
+
+        public override void Start()
+        {
+            
+        }
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            
+        }
+        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            Game1.instance.GraphicsDevice.Clear(Color.Black);
+        }
+        public override void Terminate()
+        {
+
+        }
+    }
+}
